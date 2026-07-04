5892840ecace94e3f1cab55540235f097c59ee87
uoa2026cs <298550629+uoa2026cs@users.noreply.github.com>
2026-07-05 10:47:59 +1200

    mobilenetv3 layer sweep, 40 rounds berts, notebook

---
+++ bert_layers_40/bert_layers_40.sh
@@ -0,0 +1,5 @@
+flwr federation simulation-config --num-supernodes 10 --client-resources-num-cpus 1 --client-resources-num-gpus 1 --init-args-num-cpus 1 --init-args-num-gpus 1
+for v in -1 0 1 2 3; do
+  sh mobilebert.sh bert_layers_40_$v "tune_layers=$v num-server-rounds=40 noise-multiplier=1.1 learning-rate=0.00002 batch-size=32"
+done
+sh poweroff.sh
