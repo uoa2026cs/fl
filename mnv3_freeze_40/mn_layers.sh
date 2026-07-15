d512a37256a66517ca78898173d70d42b3822f15
uoa2026cs <298550629+uoa2026cs@users.noreply.github.com>
2026-07-16 11:31:43 +1200

    fixed MobileNetV3 40 rounds, budget estimation

---
+++ mnv3_freeze_40/mn_layers.sh
@@ -0,0 +1,5 @@
+flwr federation simulation-config --num-supernodes 10 --client-resources-num-cpus 1 --client-resources-num-gpus 1 --init-args-num-cpus 1 --init-args-num-gpus 1
+for v in -1 0 1 2 3; do
+  sh mobilenetv3.sh mn_layers_$v "tune_layers=$v num-server-rounds=40 noise-multiplier=1.1 max-grad-norm=1 learning-rate=0.01 batch-size=32"
+done
+sh poweroff.sh
